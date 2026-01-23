91d94479869d306c9d3cd5b8ed289f6929ca01cf
Isaiah Else <isaiahelse@MacBookPro.wlan.nwciowa.edu>
2026-01-23 11:45:38 -0600

    Secure application with Clerk authentication

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,21 @@
 
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // By default, clerkMiddleware does not protect any routes. 
 // All routes are public and you must opt-in to protection for specific routes.
 // See https://clerk.com/docs/references/nextjs/clerk-middleware for more information about configuring your Middleware
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+    '/sign-in(.*)',
+    '/sign-up(.*)',
+    '/api/webhook(.*)'
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+    if (!isPublicRoute(req)) {
+        await auth.protect();
+    }
+});
 
 export const config = {
     matcher: [
